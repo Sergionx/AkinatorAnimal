105be309243d23b7d414dcf8099cb7f9062df453
Sergionx <sergio180203@gmail.com>
2022-03-18 18:13:59 -0400

    Primera implementación de adivinar animal

--- src/Interfaz/Interfaz.java
+++ src/Interfaz/Interfaz.java
@@ -5,6 +5,8 @@
 package Interfaz;
 
 import akinator.ArbolBinarioDesicion;
+import akinator.Nodo;
+import akinator.StringUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -21,6 +23,14 @@ public class Interfaz extends javax.swing.JFrame {
      * Creates new form Interfaz
      */
     public static ArbolBinarioDesicion arbolBinarioDesicion = new ArbolBinarioDesicion();
+    public static Nodo pActual = null;
+    public static Nodo nuevoAnimal = null;
+    public static Nodo nuevaPregunta = null;
+    
+    public static boolean jugar = false;
+    public static boolean equivocacion = false;
+    public static boolean correccion = false;
+    public static boolean respuestaCorreccion = false;
     
     public Interfaz() {
         initComponents();
@@ -37,6 +47,11 @@ public class Interfaz extends javax.swing.JFrame {
 
         jPanel1 = new javax.swing.JPanel();
         LeerTxt = new javax.swing.JButton();
+        Jugar = new javax.swing.JButton();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        MensajeJugar = new javax.swing.JTextArea();
+        EnviarRespuesta = new javax.swing.JButton();
+        Respuesta = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -51,7 +66,30 @@ public class Interfaz extends javax.swing.JFrame {
         });
         jPanel1.add(LeerTxt, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, -1, -1));
 
-        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 400, 300));
+        Jugar.setText("Empezar a Jugar");
+        Jugar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JugarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(Jugar, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, -1, -1));
+
+        MensajeJugar.setColumns(20);
+        MensajeJugar.setRows(5);
+        jScrollPane1.setViewportView(MensajeJugar);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 130, 310, 150));
+
+        EnviarRespuesta.setText("Enviar Respuesta");
+        EnviarRespuesta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                EnviarRespuestaActionPerformed(evt);
+            }
+        });
+        jPanel1.add(EnviarRespuesta, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 300, -1, -1));
+        jPanel1.add(Respuesta, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 300, 140, -1));
+
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 750, 370));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -90,6 +128,142 @@ public class Interfaz extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_LeerTxtActionPerformed
 
+    private void JugarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JugarActionPerformed
+        MensajeJugar.setText("¿Estás listo para jugar una ronda? \n");
+        jugar = true;
+        
+    }//GEN-LAST:event_JugarActionPerformed
+
+    private void EnviarRespuestaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EnviarRespuestaActionPerformed
+       String respuesta = Respuesta.getText();
+        if (respuestaCorreccion) { //Recibo la respuesta a mi pregunta de correción
+            if ("si".equals(respuesta.trim().toLowerCase())) {
+                nuevaPregunta.setHijoIzq(pActual);
+                nuevaPregunta.setHijoDer(nuevoAnimal);
+            } else if ("no".equals(respuesta.trim().toLowerCase())) {
+                nuevaPregunta.setHijoIzq(nuevoAnimal);
+                nuevaPregunta.setHijoDer(pActual);
+            } else {
+                JOptionPane.showMessageDialog(null, "Por favor escriba una respuesta valida");
+                return;
+            }
+            
+            Nodo padre = arbolBinarioDesicion.Padre(pActual, arbolBinarioDesicion.getRoot()); //Busco el padre del animal en el que fallé para cambiar su hijo
+            if (padre.getHijoIzq().getData().equals(pActual.getData())) {
+                padre.setHijoIzq(nuevaPregunta);
+            } else {
+                padre.setHijoDer(nuevaPregunta);
+            }
+            MensajeJugar.append("¡Muchas gracias!, ahora soy mucho más inteligente que antes. \n");
+            respuestaCorreccion = false;
+            Respuesta.setText("");
+            return;
+        }
+       
+       
+        if (correccion) { // Pregunto qué caracteriza el nuevo animal
+            MensajeJugar.append("Si el animal fuera un " + StringUtils.Capitalize(nuevoAnimal.getData()) + ", ¿cuál sería la respuesta a la pregunta \n");
+            
+            correccion = false;
+            respuestaCorreccion = true;
+            nuevaPregunta = new Nodo(respuesta);
+            return;
+        }
+       
+       if (equivocacion) { //No adivinó el animal
+            nuevoAnimal = new Nodo(respuesta);
+            MensajeJugar.append("¿Qué diferencia a un " + StringUtils.Capitalize(pActual.getData()) + 
+                    " y un " + StringUtils.Capitalize(respuesta) + "? \n");
+            
+            equivocacion = false;
+            correccion = true;
+            Respuesta.setText("");
+            return;
+        }
+        
+        if (!jugar) { //El juego ya se terminó
+            JOptionPane.showMessageDialog(null, "Necesita darle al botón de empezar a jugar para volver a jugar");
+            return;
+        }
+       
+        if ("".equals(MensajeJugar.getText())) {
+            JOptionPane.showMessageDialog(null, "Necesita darle al botón de empezar a jugar primero");
+            return;
+        }
+        
+        if (arbolBinarioDesicion.getRoot() == null) { //No se ha cargado el arbol
+            JOptionPane.showMessageDialog(null, "Asegúrese de leer un árbol antes de empezar a jugar");
+            return;
+        }
+
+        if ("¿Estás listo para jugar una ronda? \n".equals(MensajeJugar.getText())) {
+            if ("si".equals(respuesta.trim().toLowerCase())) {
+                pActual = arbolBinarioDesicion.getRoot();
+                String pregunta = "¿" + StringUtils.Capitalize(pActual.getData()) + "?\n";
+                
+                MensajeJugar.append(pregunta);
+                Respuesta.setText("");
+                return;
+            } else if ("no".equals(respuesta.trim().toLowerCase())) {
+                MensajeJugar.append("Vuelve cuando estés preparado para perder.");
+                Respuesta.setText("");
+                return;
+            } else {
+                JOptionPane.showMessageDialog(null, "Por favor escriba una respuesta valida");
+                Respuesta.setText("");
+                return;
+            }
+        }
+        
+        if (arbolBinarioDesicion.isAnaimal(pActual)) {
+            if ("si".equals(respuesta.trim().toLowerCase())) {
+                MensajeJugar.append("¡Qué fácil, ponlo más difícil la próxima vez! \n");
+                
+                jugar = false;
+                Respuesta.setText("");
+                return;
+            } else if ("no".equals(respuesta.trim().toLowerCase())) { //Agregar nuevo animal
+                 MensajeJugar.append("¿Qué animal era? \n");
+                 
+                 Respuesta.setText("");
+                 equivocacion = true;
+                return;
+            } else {
+                JOptionPane.showMessageDialog(null, "Por favor escriba una respuesta valida");
+                Respuesta.setText("");
+                return;
+            }
+        }
+        
+        
+        String pregunta;
+        if ("si".equals(respuesta.trim().toLowerCase())) {
+                pActual = pActual.getHijoDer();
+                if (arbolBinarioDesicion.isAnaimal(pActual)) {
+                    pregunta = "¿Es un " +  StringUtils.Capitalize(pActual.getData())+ "?\n";
+                } else {
+                    pregunta = "¿" +  StringUtils.Capitalize(pActual.getData()) + "?\n";
+                }
+                MensajeJugar.append(pregunta);
+                
+            } else if ("no".equals(respuesta.trim().toLowerCase())) {
+                pActual = pActual.getHijoIzq();
+                
+                if (arbolBinarioDesicion.isAnaimal(pActual)) {
+                    pregunta = "¿Es un " +  StringUtils.Capitalize(pActual.getData()) + "?\n";
+                } else {
+                    pregunta = "¿" +  StringUtils.Capitalize(pActual.getData()) + "?\n";                
+                }
+                MensajeJugar.append(pregunta);
+            } else {
+                JOptionPane.showMessageDialog(null, "Por favor escriba una respuesta valida");
+                return;
+        }
+        
+        
+        Respuesta.setText("");
+    }//GEN-LAST:event_EnviarRespuestaActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -126,7 +300,12 @@ public class Interfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton EnviarRespuesta;
+    private javax.swing.JButton Jugar;
     private javax.swing.JButton LeerTxt;
+    private javax.swing.JTextArea MensajeJugar;
+    private javax.swing.JTextField Respuesta;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
